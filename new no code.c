c8733b3f42a5f340c45fb591b4c50e33656f58f6
muid7171 <muid15-6999@diu.edu.bd>
2019-08-31 10:05:56 +0600

    first  commit

---
+++ new no code.c
@@ -0,0 +1,6 @@
+echo "# testgitpc" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/muid7171/testgitpc.git
+git push -u origin master
